4394d6380fde0d52a65da327c5bebc58bca7e8d0
Dave Berenbaum <david_berenbaum@brown.edu>
2016-01-19 14:30:54 -0500

    adds inputs and dynamic charts

--- web/js/charts.js
+++ web/js/charts.js
@@ -14,13 +14,13 @@ d3.text("../data_files/FDR.csv", function(text) {
 
     var fdrData = ["Fisher's Discriminant Ratio"];
     var xAxisVals = ['x'];
-    var numFeatures = 1;
+    var featureNum = 1;
 
     d3.csv.parseRows(text, function(row) {
         fdrData.push(+row[1]);
-        xAxisVals.push(numFeatures);
+        xAxisVals.push(featureNum);
         features.push(row[0]);
-        numFeatures++;
+        featureNum++;
     });
 
     c3.generate({
@@ -62,145 +62,202 @@ d3.text("../data_files/FDR.csv", function(text) {
             show: false
         }
     });
-
-    corrMat();
-    confMat();
-    scoreHist();
-
 });
 
-//Correlation coefficient matrix.
-function corrMat() {
+//Initial correlation coefficient matrix.
+var corrHeatmap;
+var corrData = [];
+var corrGridSize = (heatmapWidth - padding * 2) / (numFeatures + 2);
+var legendWidth = (heatmapWidth - padding * 2) / colors.length;
 
-    var corrData = [];
-    var gridSize = (heatmapWidth - padding * 2) / (features.length + 2);
-    var legendWidth = (heatmapWidth - padding * 2) / colors.length;
+var corrColors = d3.scale.quantile()
+    .domain([-1,1])
+    .range(colors);
 
-    var corrColors = d3.scale.quantile()
-        .domain([-1,1])
-        .range(colors);
+var corrSvg = d3.select("#corr").append("svg")
+    .attr("width", heatmapWidth)
+    .attr("height", heatmapWidth)
+    .append("g")
+    .attr("transform", "translate(" + padding + "," + padding + ")");
 
-    var corrSvg = d3.select("#corr").append("svg")
-        .attr("width", heatmapWidth)
-        .attr("height", heatmapWidth)
-        .append("g")
-        .attr("transform", "translate(" + padding + "," + padding + ")");
+var corrLegend = corrSvg.selectAll(".legend")
+    .data([-1].concat(corrColors.quantiles()), function(d) { return d; });
 
-    var corrLegend = corrSvg.selectAll(".legend")
-        .data([-1].concat(corrColors.quantiles()), function(d) { return d; });
+corrLegend.enter().append("g");
 
-    corrLegend.enter().append("g");
+corrLegend.append("rect")
+    .attr("x", function(d, i) { return i * legendWidth; })
+    .attr("y", heatmapWidth - padding - corrGridSize * 2)
+    .attr("width", legendWidth)
+    .attr("height", corrGridSize)
+    .style("fill", function(d, i) { return colors[i]; });
 
-    corrLegend.append("rect")
-        .attr("x", function(d, i) { return i * legendWidth; })
-        .attr("y", heatmapWidth - padding - gridSize * 2)
-        .attr("width", legendWidth)
-        .attr("height", gridSize)
-        .style("fill", function(d, i) { return colors[i]; });
+corrLegend.append("text")
+    .attr("x", function(d, i) { return legendWidth * i; })
+    .attr("y", heatmapWidth - padding)
+    .attr("font-size", corrGridSize + "px")
+    .text(function(d) { return "≥ " + d.toFixed(2); });
 
-    corrLegend.append("text")
-        .text(function(d) { return "≥ " + d.toFixed(2); })
-        .attr("x", function(d, i) { return legendWidth * i; })
-        .attr("y", heatmapWidth - padding)
-
+d3.text("../data_files/corr_coef_" + numFeatures + ".csv", function(text) {
     var rowNum = 0
-    d3.text("../data_files/corr_coef_" + numFeatures + ".csv", function(text) {
-        d3.csv.parseRows(text, function(row) {
-            for(var colNum = 0; colNum < row.length; colNum++) {
-                corrData.push({"row": rowNum, "col": colNum, "val": +row[colNum]});
-            }
-            rowNum++;
-        });
-
-        var heatmap = corrSvg.selectAll()
-            .data(corrData, function(d) { return d.row + ":" + d.col; });
-
-        heatmap.enter().append("rect")
-            .attr("x", function(d) { return d.col * gridSize; })
-            .attr("y", function(d) { return d.row * gridSize; })
-            .attr("width", gridSize)
-            .attr("height", gridSize)
-            .style("fill", function(d) { return corrColors(d.val); });
-
-        heatmap.transition()
-            .style("opacity", function(d) { 
-                if(d.row >= numFeatures || d.col >= numFeatures) {
-                    return 0.2;
-                } else {
-                    return 1; 
-                }
-            });
+    d3.csv.parseRows(text, function(row) {
+        for(var colNum = 0; colNum < row.length; colNum++) {
+            corrData.push({"row": rowNum, "col": colNum, "val": +row[colNum]});
+        }
+        rowNum++;
     });
+
+    corrHeatmap = corrSvg.selectAll()
+        .data(corrData, function(d) { return d.row + ":" + d.col; });
+
+    corrHeatmap.enter().append("rect")
+        .attr("x", function(d) { return d.col * corrGridSize; })
+        .attr("y", function(d) { return d.row * corrGridSize; })
+        .attr("width", corrGridSize)
+        .attr("height", corrGridSize)
+        .style("fill", function(d) { return corrColors(d.val); });
+
+    corrMatUpdate();
+});
+
+//Initial score histogram.
+var scoreHistChart = c3.generate({
+    bindto: "#histChart",
+    size: {
+        width: heatmapWidth
+    },
+    data: {
+        columns: [],
+        type: "bar"
+    },
+    tooltip: {
+        format: {
+            title: function(x) { return "Classification Score [%]: " + x; },
+            value: function(value, ratio, id, index) { return value.toFixed(2); }
+        }
+    },
+    axis: {
+        x: {
+            label: {
+                text: "Classification Score [%]",
+                position: "outer-center"
+            }
+        },
+        y: {
+            label: {
+                text: "Fraction of Simulations [%]",
+                position: "outer-middle"
+            }
+        }
+    },
+    legend: {
+        show: false
+    }
+});
+
+scoreHistUpdate();
+
+//Initial confusion matrix.
+var confHeatmap;
+var confGridSize = (heatmapWidth - padding * 2) / (classes.length + 2);
+var legendWidth = (heatmapWidth - padding * 2) / colors.length;
+
+var confColors = d3.scale.quantile()
+    .domain([0,0.2])
+    .range(colors);
+
+var confSvg = d3.select("#conf").append("svg")
+    .attr("width", heatmapWidth)
+    .attr("height", heatmapWidth)
+    .append("g")
+    .attr("transform", "translate(" + (padding + confGridSize) + "," + 
+            (padding + confGridSize) + ")");
+
+confSvg.append("text")
+    .attr("text-anchor", "middle")
+    .attr("x", (heatmapWidth - (padding * 2) - (confGridSize * 2)) / 2)
+    .attr("y", confGridSize * -2/3)
+    .text("Predicted");
+
+confSvg.append("text")
+    .attr("text-anchor", "middle")
+    .attr("x", (heatmapWidth - (padding * 2) - (confGridSize * 2)) / -2)
+    .attr("y", confGridSize * -2/3)
+    .attr("transform", "rotate(-90)")
+    .text("True");
+
+confSvg.selectAll(".rowLabel")
+    .data(classes)
+    .enter().append("text")
+      .text(function(d) { return d; })
+      .attr("x", 0)
+      .attr("y", function(d, i) { return i * confGridSize; })
+      .style("text-anchor", "end")
+      .attr("transform", "translate(" + (confGridSize * -1/3) + "," + (confGridSize / 2) + ")");
+
+var confLegend = confSvg.selectAll(".legend")
+    .data([0].concat(confColors.quantiles()), function(d) { return d; });
+
+confLegend.enter().append("g");
+
+confLegend.append("rect")
+    .attr("x", function(d, i) { return i * legendWidth; })
+    .attr("y", heatmapWidth - padding - (confGridSize * 1.75))
+    .attr("width", legendWidth)
+    .attr("height", confGridSize / 2)
+    .style("fill", function(d, i) { return colors[i]; });
+
+confLegend.append("text")
+    .attr("x", function(d, i) { return legendWidth * i; })
+    .attr("y", heatmapWidth - padding - confGridSize)
+    .text(function(d) { return "≥ " + d.toFixed(2); });
+
+confSvg.selectAll(".colLabel")
+    .data(classes)
+    .enter().append("text")
+      .text(function(d) { return d; })
+      .attr("x", function(d, i) { return i * confGridSize; })
+      .attr("y", 0)
+      .style("text-anchor", "end")
+      .attr("transform", "translate(" + (confGridSize / 2) + "," + (confGridSize * -1/3) + ")");
+
+var confData = [];
+for(var rowNum = 0; rowNum < classes.length; rowNum ++) {
+    for(var colNum = 0; colNum < classes.length; colNum++) {
+        confData.push({"row": rowNum, "col": colNum});
+    }
 }
 
-//Confusion matrix.
-function confMat() {
+var confHeatmap = confSvg.selectAll(".cells")
+    .data(confData, function(d) { return d.row + ":" + d.col; });
 
-    var confData = [];
-    var gridSize = (heatmapWidth - padding * 2) / (classes.length + 2);
-    var legendWidth = (heatmapWidth - padding * 2) / colors.length;
+confHeatmap.enter().append("rect")
+    .attr("class", "cells")
+    .attr("x", function(d) { return d.col * confGridSize; })
+    .attr("y", function(d) { return d.row * confGridSize; })
+    .attr("width", confGridSize)
+    .attr("height", confGridSize)
+    .style("fill", function(d) { return confColors(0); });
 
-    var confColors = d3.scale.quantile()
-        .domain([0,0.2])
-        .range(colors);
+confMatUpdate();
 
-    var confSvg = d3.select("#conf").append("svg")
-        .attr("width", heatmapWidth)
-        .attr("height", heatmapWidth)
-        .append("g")
-        .attr("transform", "translate(" + (padding + gridSize) + "," + 
-                (padding + gridSize) + ")");
+//Update correlation matrix.
+function corrMatUpdate() {
+    corrHeatmap.transition()
+        .style("opacity", function(d) { 
+            if(d.row >= numFeatures || d.col >= numFeatures) {
+                return 0.2;
+            } else {
+                return 1; 
+            }
+        });
+}
 
-    confSvg.append("text")
-        .attr("text-anchor", "middle")
-        .attr("x", (heatmapWidth - (padding * 2) - (gridSize * 2)) / 2)
-        .attr("y", gridSize * -2/3)
-        .text("Predicted");
-
-    confSvg.append("text")
-        .attr("text-anchor", "middle")
-        .attr("x", (heatmapWidth - (padding * 2) - (gridSize * 2)) / -2)
-        .attr("y", gridSize * -2/3)
-        .attr("transform", "rotate(-90)")
-        .text("True");
-
-    confSvg.selectAll(".rowLabel")
-        .data(classes)
-        .enter().append("text")
-          .text(function(d) { return d; })
-          .attr("x", 0)
-          .attr("y", function(d, i) { return i * gridSize; })
-          .style("text-anchor", "end")
-          .attr("transform", "translate(" + (gridSize * -1/3) + "," + (gridSize / 2) + ")");
-
-    var confLegend = confSvg.selectAll(".legend")
-        .data([0].concat(confColors.quantiles()), function(d) { return d; });
-
-    confLegend.enter().append("g");
-
-    confLegend.append("rect")
-        .attr("x", function(d, i) { return i * legendWidth; })
-        .attr("y", heatmapWidth - padding - (gridSize * 1.75))
-        .attr("width", legendWidth)
-        .attr("height", gridSize / 2)
-        .style("fill", function(d, i) { return colors[i]; });
-
-    confLegend.append("text")
-        .text(function(d) { return "≥ " + d.toFixed(2); })
-        .attr("x", function(d, i) { return legendWidth * i; })
-        .attr("y", heatmapWidth - padding - gridSize)
-
-    confSvg.selectAll(".colLabel")
-        .data(classes)
-        .enter().append("text")
-          .text(function(d) { return d; })
-          .attr("x", function(d, i) { return i * gridSize; })
-          .attr("y", 0)
-          .style("text-anchor", "end")
-          .attr("transform", "translate(" + (gridSize / 2) + "," + (gridSize * -1/3) + ")");
-
-    var rowNum = 0
+//Update confusion matrix.
+function confMatUpdate() {
     d3.text("../data_files/confusion_m_nrf" + numFeatures + "_bw" + bw + ".csv", function(text) {
+        var confData = [];
+        var rowNum = 0
         d3.csv.parseRows(text, function(row) {
             for(var colNum = 0; colNum < row.length; colNum++) {
                 confData.push({"row": rowNum, "col": colNum, "val": +row[colNum]});
@@ -208,24 +265,16 @@ function confMat() {
             rowNum++;
         });
 
-        var heatmap = confSvg.selectAll()
+        var confHeatmap = confSvg.selectAll(".cells")
             .data(confData, function(d) { return d.row + ":" + d.col; });
 
-        heatmap.enter().append("rect")
-            .attr("x", function(d) { return d.col * gridSize; })
-            .attr("y", function(d) { return d.row * gridSize; })
-            .attr("width", gridSize)
-            .attr("height", gridSize)
-            .style("fill", function(d) { return colors[0]; });
-        
-        heatmap.transition()
+        confHeatmap.transition()
             .style("fill", function(d) { return confColors(d.val); });
     });
 }
 
-
-//Histogram.
-function scoreHist() {
+//Update score histogram.
+function scoreHistUpdate() {
     d3.text("../data_files/histogram_nrf" + numFeatures + "_bw" + bw + ".csv", function(text) {
 
         var histData = ["Fraction of Simulations [%]"];
@@ -234,38 +283,25 @@ function scoreHist() {
             histData.push(+row[1]);
         });
 
-        c3.generate({
-            bindto: "#histChart",
-            size: {
-                width: heatmapWidth
-            },
-            data: {
-                columns: [histData],
-                type: "bar"
-            },
-            tooltip: {
-                format: {
-                    title: function(x) { return "Classification Score [%]: " + x; },
-                    value: function(value, ratio, id, index) { return value.toFixed(2); }
-                }
-            },
-            axis: {
-                x: {
-                    label: {
-                        text: "Classification Score [%]",
-                        position: "outer-center"
-                    }
-                },
-                y: {
-                    label: {
-                        text: "Fraction of Simulations [%]",
-                        position: "outer-middle"
-                    }
-                }
-            },
-            legend: {
-                show: false
-            }
+        scoreHistChart.load({
+            columns: [histData],
         });
     });
+}
+
+//Features slider action.
+function featuresUpdate(val) {
+    document.getElementById("featVal").innerHTML = val;
+    numFeatures = parseInt(val);
+    corrMatUpdate();
+    scoreHistUpdate();
+    confMatUpdate();
+}
+
+//Bandwidth slider action.
+function bwUpdate(val) {
+    document.getElementById("bwVal").innerHTML = val;
+    bw = val;
+    scoreHistUpdate();
+    confMatUpdate();
 }
